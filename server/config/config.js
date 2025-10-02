0864264415ff5cb4877413a9399f1428f5b18a00
Mohammed Anees <mohammedanees0606@gmail.com>
2025-10-02 23:29:52 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -4,9 +4,8 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://host.docker.internal:27017/todoappss', {
-    useNewUrlParser: true,
-   
+ await mongoose.connect('mongodb://127.0.0.1:27017/todoappss', {
+  useNewUrlParser: true,
 });
     console.log(" MongoDB connected");
   } catch (err) {
