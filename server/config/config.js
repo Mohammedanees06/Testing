ae25ad5b7bee14aca7ee4330170997ae6b9caf41
Mohammed Anees <mohammedanees0606@gmail.com>
2025-10-02 23:44:29 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -4,10 +4,11 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
- await mongoose.connect('mongodb://host.docker.internal:27017/todoappss',  {
-  useNewUrlParser: true,
-});
-    console.log(" MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
     process.exit(1);
@@ -15,23 +16,3 @@ const connectDB = async () => {
 };
 
 export default connectDB;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
